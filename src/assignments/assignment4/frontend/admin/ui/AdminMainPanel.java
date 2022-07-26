7dc982e1224de79ba7756cbc69d204bfdcf32dbd
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-07-26 15:16:11 +0700

    Adding AdminMainPanel and UserMainPanel

---
+++ src/assignments/assignment4/frontend/admin/ui/AdminMainPanel.java
@@ -0,0 +1,17 @@
+package assignments.assignment4.frontend.admin.ui;
+
+import assignments.assignment4.frontend.HomeGUI;
+import assignments.assignment4.frontend.SistemPanel;
+
+public class AdminMainPanel extends SistemPanel {
+
+    public AdminMainPanel(HomeGUI main) {
+        super(main);
+        // TODO
+    }
+
+    @Override
+    public void refresh() {
+        // ignored
+    }
+}
---
+++ src/assignments/assignment4/frontend/user/ui/UserMainPanel.java
@@ -0,0 +1,17 @@
+package assignments.assignment4.frontend.user.ui;
+
+import assignments.assignment4.frontend.HomeGUI;
+import assignments.assignment4.frontend.SistemPanel;
+
+public class UserMainPanel extends SistemPanel {
+
+    public UserMainPanel(HomeGUI main) {
+        super(main);
+        // TODO
+    }
+
+    @Override
+    public void refresh() {
+        // ignored
+    }
+}
